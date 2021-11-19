e67e201b62352aff817c7a7d5823a56cdccb3060
Frortate <danya.seleznev.1990@mail.ru>
2021-11-19 18:30:19 +0300

    log

---
+++ SE Селезнёв Д.А. 3-41xx  (Курсовое приложение)/View/PageLoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using SE_Селезнёв_Д.А._3_41xx___Курсовое_приложение_.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SE_Селезнёв_Д.А._3_41xx___Курсовое_приложение_.View
+{
+    class PageLoginViewModel
+    {
+        IDbCrud crud;
+    }
+}
